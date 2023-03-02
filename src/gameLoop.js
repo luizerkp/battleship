672ae3720fe1382d218d6c12495b9dacfa8c97db
Luis Tamarez <luistamarez@hotmail.com>
2023-03-01 21:17:24 -0800

    Update index.html   Add gameLoop module

--- src/gameLoop.js
+++ src/gameLoop.js
@@ -1,1 +1,16 @@
 import Player from "./Player";
+
+const gameLoop = (() => {
+  const start = (name) => {
+    const humanPlayer = new Player(name);
+    const computer = new Player("computer");
+    computer.initAutoShipPlacement();
+    // return a prompt human place to placeships or have them place randomly;
+  };
+
+  return {
+    start,
+  };
+})();
+
+export default gameLoop;
